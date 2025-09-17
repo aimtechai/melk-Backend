af6bd77d9a820b5eecc6a21620f118a0d62d17b6
Muhammad Taha <mtaha.developer@gmail.com>
2025-09-17 17:11:43 -0400

    add routes and devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +12,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_for :users, controllers: {
+        sessions: "api/v1/sessions",
+        registrations: "api/v1/registrations",
+        passwords: "api/v1/passwords"
+      }
+
+      devise_scope :user do
+        get "users/current", to: "sessions#current_user_info"
+        post "passwords/send_otp", to: "passwords#send_otp"
+        post "passwords/verify_otp", to: "passwords#verify_otp"
+        put "passwords/reset", to: "passwords#reset"
+      end
+    end
+  end
 end
