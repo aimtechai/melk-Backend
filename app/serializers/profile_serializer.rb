89e38d8335ef5a17bed47b37c0c5f135b31ab675
Muhammad Taha <mtaha.developer@gmail.com>
2025-09-19 15:51:32 -0400

    refactor profile_serializer.rb

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,10 +1,12 @@
 class ProfileSerializer
   include JSONAPI::Serializer
+  include Rails.application.routes.url_helpers
+
   attributes :first_name, :last_name, :phone, :address,
              :emergency_contact_name, :emergency_contact_phone,
              :tenant_settings
 
-  attribute :avatar_url do |profile|
-    Rails.application.routes.url_helpers.url_for(profile.avatar) if profile.avatar.attached?
+  attribute :avatar_url do |profile, params|
+    profile.avatar.attached? ? rails_blob_url(profile.avatar, host: params[:host]) : nil
   end
 end
